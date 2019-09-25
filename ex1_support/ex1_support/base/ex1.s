8562908a1062096a268f1ccf09d7ba84126c6160
davidbp13 <davidbp.13@gmail.com>
2019-09-25 18:53:22 +0200

    Ex1 Solution added

--- ex1_support/ex1_support/base/ex1.s
+++ ex1_support/ex1_support/base/ex1.s
@@ -4,8 +4,8 @@
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
-  // Exception vector table
-  // This table contains addresses for all exception handlers
+	// Exception vector table
+	// This table contains addresses for all exception handlers
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
@@ -69,86 +69,152 @@
 	      .long   dummy_handler
 	      .long   dummy_handler
 
-	      .section .text
+	    .section .text
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
 	// Reset handler
-  // The CPU will start executing here after a reset
+	// The CPU will start executing here after a reset
 	//
 	/////////////////////////////////////////////////////////////////////////////
 
-	      .globl  _reset
-	      .type   _reset, %function
+	    .globl  _reset
+	    .type   _reset, %function
         .thumb_func
 _reset: 
-		// switch the 13 bit of the CMU register to 1 to enable clock//
-		  // load CMU base address
-		  ldr r1, =CMU_BASE
-		  
-		  //load current value of HFPERCLKEN ENABLE
-		  ldr r2,[r1,#CMU_HFPERCLKEN0]
-		  mov r3,#1
-		  lsl r3,r3,#13
-		  orr r2,r2,r3
-		  str r2, [r1,#CMU_HFPERCLKEN0]
-		  //FINISH OF FIRST STEP
-		  
-		  // now with GPIO_CTRL
-		  ldr r1, =GPIO_PA_BASE
-		  mov r3,#1   //MOVE VALUE 2 TO R3
-		  str r3, [r1,#GPIO_CTRL] //STORE VALUE 2 TO GPIO_PA_CTRL
-		  
-		  //now with GPI_PA_MODEH
-		  ldr r1, =GPIO_PA_BASE
-		  ldr r3,=0x55555555   //MOVE VALUE 5555555 TO R3
-		  str r3, [r1,#GPIO_MODEH] //STORE VALUE 5555555 TO GPIO_MODEH
-		  
-		  //now with GPIO_PA_DOUT
-		  
-		  ldr r1, =GPIO_PA_BASE
-		  ldr r3,=0x0f00   //MOVE VALUE f0 TO R3
-		  str r3, [r1,#GPIO_DOUT] //STORE VALUE f0 TO GPIO_DOUT
-		  
-		  //now with GPIO_PC_MODEL
-		  ldr r1, =GPIO_PC_BASE
-		  ldr r3,=0x33333333   //MOVE VALUE 3333333 TO R3
-		  str r3, [r1,#GPIO_MODEL] //STORE VALUE 333333 TO GPIO_PC_MODEL
-		  
-		  //now with GPIO_PC_DOUT
-		  ldr r1, =GPIO_PC_BASE
-		  ldr r3,=0xff   //MOVE VALUE ff TO R3
-		  str r3, [r1,#GPIO_DOUT] //STORE VALUE ff TO GPIO_PC_DOUT
-		  
-		  
-		  //now reading from GPIO_PC_DIN
-		  
-		  ldr r1, =GPIO_PC_BASE
-		 loop1:
-		  ldr r2, [r1,#GPIO_DIN] //load the value to r2
-		  lsl r2,r2,#8
-		  ldr r3, =GPIO_PA_BASE
-		  str r2, [r3,#GPIO_DOUT]
-	      b loop1
-	      
+
+		ldr r8, =GPIO_PA_BASE				//Register for LEDs
+		ldr r9, =GPIO_PC_BASE				//Register for Buttons
+		
+		//====== ENABLE CMU FOR GPIO ======//
+		
+		ldr r0, =CMU_BASE					//Load base addresse of CMU
+		ldr r1, [r0, #CMU_HFPERCLKEN0]		//Load content of HFPERCLKEN register
+		mov r2, #1
+		lsl r2, r2, #CMU_HFPERCLKEN0_GPIO	//Shift left to set 13th bit
+		orr r2, r2, r1						//Set 13th bit wrt content of HFPERCLKEN register
+		str r2, [r0, #CMU_HFPERCLKEN0]		//Store the new content to HFPERCLKEN register
+		
+		//====== SETUP GPIO PINS =====//
+		
+		mov r1, #0x2
+		str r1, [r8, #GPIO_CTRL]			//Set high drive strength
+		
+		ldr r1, =0x55555555
+		str r1, [r8, #GPIO_MODEH]			//Set pins 8-15 (LEDs) to output 
+		
+		ldr r1, =0x33333333
+		str r1, [r9, #GPIO_MODEL]			//Set pins 0-7 (Buttons) to input
+		mov r1, 0xff
+		str r1, [r9, #GPIO_DOUT]			//Enable internal pull-up
+		
+		//====== TURN ON AND OFF LEDS ======//
+		
+		ldr r1, =0x0						//Value to write to LEDs
+		lsl r1, r1, #8						//Shift 8 bits to from least significant to most significant
+		str r1, [r8, #GPIO_DOUT]			//Write value to LEDs
+		ldr r2, =0x7a1200					//Counter capacity
+		loop:
+			sub r2, r2, #1					//Decrement counter
+			cmp r2, #0						//Compare counter
+			bne loop						//If is not equal to 0 goto loop
+		mov r1, 0xff						//Else turn off LEDs
+		lsl r1, r1, #8
+		str r1, [r8, #GPIO_DOUT]
+		
+		
+		//====== POLL ======//
+		
+		mov r7, #0b11111111					//Register for keeping previous Buttons state
+		poll:
+			ldr r0, [r9, #GPIO_DIN]			//Read state of Buttons
+			cmp r0, r7						//Compare current state with previous
+			beq poll						//If are equal goto poll
+			
+			cmp r0, #0b11111110				//Check if Left bnt pressed
+			beq shift_left
+			cmp r0, #0b11111011				//Check if Right bnt pressed
+			beq shift_right
+			cmp r0, #0b11111101				//Check if Up bnt pressed
+			beq vol_up
+			cmp r0, #0b11110111				//Check if Down bnt pressed
+			beq vol_down
+			cmp r0, #0b11101111
+			beq switch5
+			cmp r0, #0b11011111
+			beq switch6
+			cmp r0, #0b10111111
+			beq switch7
+			cmp r0, #0b01111111
+			beq switch8
+			
+			
+			b update_prev_state
+			
+		
+		update_prev_state:
+			mov r7, r0						//Save previous state of the Buttons
+			b poll
+		
+		shift_left:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			lsr r2, r2, #8					//Shift 8 bits to from most significant to least significant
+			lsr r1, r2, #1					//Shift right 1 bit
+			add r1, r1, #0x80				//Turn off one LED from the left
+			lsl r1, r1, #8
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			b update_prev_state
+		shift_right:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			lsr r2, r2, #8					//Shift 8 bits to from most significant to least significant
+			lsl r1, r2, #1					//Shift left 1 bit
+			add r1, r1, #0x01				//Turn off one LED from the right
+			lsl r1, r1, #8
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			b update_prev_state
+		vol_up:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			ror r1, r2, #1					//Rotate 1 bit right
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			b update_prev_state
+		vol_down:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			lsl r1, r2, #1					//Shift left 1 bit
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			b update_prev_state
+			
+		switch5:
+			ldr r2,=0x0f00
+			str r2,[r8,#GPIO_DOUT]
+			b update_prev_state
+		switch6:
+			ldr r2,=0xf000
+			str r2,[r8,#GPIO_DOUT]
+			b update_prev_state
+		switch7:
+			ldr r2,=0b1010101000000000
+			str r2,[r8,#GPIO_DOUT]
+			b update_prev_state
+		switch8:
+			ldr r2,=0b0101010100000000
+			str r2,[r8,#GPIO_DOUT]
+			b update_prev_state
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
-  // GPIO handler
-  // The CPU will jump here when there is a GPIO interrupt
+	// GPIO handler
+	// The CPU will jump here when there is a GPIO interrupt
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
 gpio_handler:  
 
-	      b .  // do nothing
+		b .  // do nothing
 	
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
 dummy_handler:  
-        b .  // do nothing
-        
-       
+		b .  // do nothing
 
--- ex1_support/ex1_support/impr/ex1.s
+++ ex1_support/ex1_support/impr/ex1.s
@@ -4,8 +4,8 @@
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
-  // Exception vector table
-  // This table contains addresses for all exception handlers
+	// Exception vector table
+	// This table contains addresses for all exception handlers
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
@@ -69,132 +69,189 @@
 	      .long   dummy_handler
 	      .long   dummy_handler
 
-	      .section .text
+	    .section .text
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
 	// Reset handler
-  // The CPU will start executing here after a reset
+	// The CPU will start executing here after a reset
 	//
 	/////////////////////////////////////////////////////////////////////////////
 
-	      .globl  _reset
-	      .type   _reset, %function
+	    .globl  _reset
+	    .type   _reset, %function
         .thumb_func
 _reset: 
 
-
-//INITIALIZATION
-
-		// switch the 13 bit of the CMU register to 1 to enable clock//
-		  // load CMU base address
-		  ldr r1, =CMU_BASE
-		  
-		  //load current value of HFPERCLKEN ENABLE
-		  ldr r2,[r1,#CMU_HFPERCLKEN0]
-		  mov r3,#1
-		  lsl r3,r3,#13
-		  orr r2,r2,r3
-		  str r2, [r1,#CMU_HFPERCLKEN0]
-		  //FINISH OF FIRST STEP
-		  
-		  // now with GPIO_CTRL
-		  ldr r1, =GPIO_PA_BASE
-		  mov r3,#1   //MOVE VALUE 2 TO R3
-		  str r3, [r1,#GPIO_CTRL] //STORE VALUE 2 TO GPIO_PA_CTRL
-		  
-		  //now with GPI_PA_MODEH
-		  ldr r1, =GPIO_PA_BASE
-		  ldr r3,=0x55555555   //MOVE VALUE 5555555 TO R3
-		  str r3, [r1,#GPIO_MODEH] //STORE VALUE 5555555 TO GPIO_MODEH
-		  
-		  //now with GPIO_PA_DOUT
-		  
-		  ldr r1, =GPIO_PA_BASE
-		  ldr r3,=0x0f00   //MOVE VALUE f0 TO R3
-		  str r3, [r1,#GPIO_DOUT] //STORE VALUE f0 TO GPIO_DOUT
-		  
-		  //now with GPIO_PC_MODEL
-		  ldr r1, =GPIO_PC_BASE
-		  ldr r3,=0x33333333   //MOVE VALUE 3333333 TO R3
-		  str r3, [r1,#GPIO_MODEL] //STORE VALUE 333333 TO GPIO_PC_MODEL
-		  
-		  //now with GPIO_PC_DOUT
-		  ldr r1, =GPIO_PC_BASE
-		  ldr r3,=0xff   //MOVE VALUE ff TO R3
-		  str r3, [r1,#GPIO_DOUT] //STORE VALUE ff TO GPIO_PC_DOUT
-		  
-		  //INIT for INTERRUPT PART STARTS HERE 
-		  
-		  //now with GPIO_EXTIPSELL
-		  ldr r1, =GPIO_BASE
-		  ldr r3,=0x22222222   //MOVE VALUE 22222222 TO R3
-		  str r3, [r1,#GPIO_EXTIPSELL] //STORE VALUE 2222222 TO GPIO_EXTIPSELL
-		  
-		  //now with GPIO_EXTIFALL
-		  ldr r1, =GPIO_BASE
-		  ldr r3,=0xff   //MOVE VALUE ff TO R3
-		  str r3, [r1,# GPIO_EXTIFALL] //STORE VALUE ff TO GPIO_EXTIFALL
-		  
-		  //now with GPIO_EXTIRISE
-		  ldr r1, =GPIO_BASE
-		  ldr r3,=0xff   //MOVE VALUE ff TO R3
-		  str r3, [r1,# GPIO_EXTIRISE] //STORE VALUE ff TO GPIO_EXTIRISE
-		  
-		  //now with GPIO_IEN
-		  ldr r1, =GPIO_BASE
-		  ldr r3,=0xff   //MOVE VALUE ff TO R3
-		  str r3, [r1,# GPIO_IEN] //STORE VALUE ff TO GPIO_IEN
-		  
-		  // ISER0
-		  ldr r1, =ISER0
-		  ldr r3,=0x802   //MOVE VALUE 802 TO R3
-		  str r3, [r1] //STORE VALUE ff TO ISER0
-		  
-		  //power opt
-		  ldr r1,=SCR
-		  ldr r2,=0x6
-		  str r2,[r1]
-		  
-		// waiting_loop:
-		 wfi
-		 	//b waiting_loop
-		 	
-	      
+		ldr r8, =GPIO_PA_BASE				//Register for LEDs
+		ldr r9, =GPIO_PC_BASE				//Register for Buttons
+		ldr r10, =GPIO_BASE					//Register for GPIOs
+		
+		//====== ENABLE CMU FOR GPIO ======//
+		
+		ldr r0, =CMU_BASE					//Load base addresse of CMU
+		ldr r1, [r0, #CMU_HFPERCLKEN0]		//Load content of HFPERCLKEN register
+		mov r2, #1
+		lsl r2, r2, #CMU_HFPERCLKEN0_GPIO	//Shift left to set 13th bit
+		orr r2, r2, r1						//Set 13th bit wrt content of HFPERCLKEN register
+		str r2, [r0, #CMU_HFPERCLKEN0]		//Store the new content to HFPERCLKEN register
+		
+		//====== SETUP GPIO PINS =====//
+		
+		mov r1, #0x1
+		str r1, [r8, #GPIO_CTRL]			//Set high drive strength
+		
+		ldr r1, =0x55555555
+		str r1, [r8, #GPIO_MODEH]			//Set pins 8-15 (LEDs) to output 
+		
+		ldr r1, =0x33333333
+		str r1, [r9, #GPIO_MODEL]			//Set pins 0-7 (Buttons) to input
+		mov r1, 0xff
+		str r1, [r9, #GPIO_DOUT]			//Enable internal pull-up
+		
+		//====== TURN ON AND OFF LEDS ======//
+		
+		ldr r1, =0x0						//Value to write to LEDs
+		lsl r1, r1, #8						//Shift 8 bits to from least significant to most significant
+		str r1, [r8, #GPIO_DOUT]			//Write value to LEDs
+		ldr r2, =0x7a1200					//Counter capacity
+		loop:
+			sub r2, r2, #1					//Decrement counter
+			cmp r2, #0						//Compare counter
+			bne loop						//If is not equal to 0 goto loop
+		mov r1, 0xff						//Else turn off LEDs
+		lsl r1, r1, #8
+		str r1, [r8, #GPIO_DOUT]
+		
+		//====== DISABLE INSTRUCTION CACHE ======//
+		
+		ldr r5, =MSC_BASE					//Load base address of MSC
+		ldr r6, [r5, #MSC_READCTRL]			//Load the content of READCTRL register
+		
+		mov r4, #1
+		lsl r4, r4, #3						//Set 4th bit
+		orr r6, r6, r4						//Set 4th bit wrt content of READCTRL register
+		
+		str r6, [r5, #MSC_READCTRL]			//Store the new content to READCTRL register
+		
+		//====== DISABLE RAM BLOCKS ======//
+	
+		ldr r5, =EMU_BASE					
+		mov r6, #0x7
+		str r6, [r5, #EMU_MEMCTRL]
+		
+		
+		//====== ENABLES DEEP SLEEP ======//
+		
+		ldr r5, =SCR						
+		mov r6, #6							
+		str r6, [r5]						
+		
+		
+		//====== ENABLE INTERRUPT FOR GPIO ======//
+	
+		ldr r1, [r10, #GPIO_IF]				//Load the source of interrupt
+		str r1, [r10, #GPIO_IFC]			//Clear interrupt
+		
+		ldr r1, =0x22222222					
+		str r1, [r10, #GPIO_EXTIPSELL]		//Select PortC for external interrupts
+		
+		mov r1, #0xff
+		str r1, [r10, #GPIO_EXTIFALL]		//Enable falling edge trigger
+		str r1, [r10, #GPIO_IEN]			//Enable external interrupt
+		
+		ldr r2, =ISER0						
+		ldr r1, =0x802						
+		str r1, [r2]						//Enable interrupt handling 
+		
+		wfi
+		
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
-  // GPIO handler
-  // The CPU will jump here when there is a GPIO interrupt
+	// GPIO handler
+	// The CPU will jump here when there is a GPIO interrupt
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
 gpio_handler:  
-
-		  //clear the interrupt 
-		  
-		  ldr r1, =GPIO_BASE
-		  ldr r2,[r1,#GPIO_IF]
-		  ldr r3,[r1,#GPIO_IFC]
-		  mov r3,r2
-		  
-		  //now read from the interrupt, place it in output
-		 
-		  ldr r1, =GPIO_PC_BASE
-		  ldr r2, [r1,#GPIO_DIN] //load the value to r2
-		  lsl r2,r2,#8
-		  ldr r3, =GPIO_PA_BASE
-		  str r2, [r3,#GPIO_DOUT]
-		  bx lr
-
-	     // b .  // do nothing
-	
+		ldr r1, [r10, #GPIO_IF]				
+		str r1, [r10, #GPIO_IFC]				
+		
+		ldr r0, [r9, #GPIO_DIN]			//Read state of Buttons
+		
+		cmp r0, #0b11111110				//Check if Left bnt pressed
+		beq shift_left
+		cmp r0, #0b11111011				//Check if Right bnt pressed
+		beq shift_right
+		cmp r0, #0b11111101				//Check if Up bnt pressed
+		beq vol_up
+		cmp r0, #0b11110111				//Check if Down bnt pressed
+		beq vol_down
+		//mouayad started here//
+		cmp r0, #0b11101111
+		beq switch5
+		cmp r0, #0b11011111
+		beq switch6
+		cmp r0, #0b10111111
+		beq switch7
+		cmp r0, #0b01111111
+		beq switch8
+		bx lr							//Return from interrupt
+		
+		
+		
+		shift_left:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			lsr r2, r2, #8					//Shift 8 bits to from most significant to least significant
+			lsr r1, r2, #1					//Shift right 1 bit
+			add r1, r1, #0x80				//Turn off one LED from the left
+			lsl r1, r1, #8
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			bx lr
+		shift_right:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			lsr r2, r2, #8					//Shift 8 bits to from most significant to least significant
+			lsl r1, r2, #1					//Shift left 1 bit
+			add r1, r1, #0x01				//Turn off one LED from the right
+			lsl r1, r1, #8
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			bx lr
+		vol_up:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			ror r1, r2, #1					//Rotate 1 bit right
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			bx lr
+		vol_down:
+			ldr r2, [r8, #GPIO_DIN]			//Load LEDs state
+			lsl r1, r2, #1					//Shift left 1 bit
+			str r1, [r8, #GPIO_DOUT]		//Write shifted state to LEDs
+			bx lr
+			
+		switch5:
+			ldr r2,=0x0f00
+			str r2,[r8,#GPIO_DOUT]
+			bx lr
+		switch6:
+			ldr r2,=0xf000
+			str r2,[r8,#GPIO_DOUT]
+			bx lr
+		switch7:
+			ldr r2,=0b1010101000000000
+			str r2,[r8,#GPIO_DOUT]
+			bx lr
+		switch8:
+			ldr r2,=0b0101010100000000
+			str r2,[r8,#GPIO_DOUT]
+			bx lr
+			
+		
+			
+			
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
 dummy_handler:  
-        b .  // do nothing
-        
-       
+		b .  // do nothing
 
