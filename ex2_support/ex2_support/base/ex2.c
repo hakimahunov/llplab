92996cbbb6099ab66077a3ead119fc419f0df732
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2019-09-28 23:30:44 +0200

    Setup DAC and Timer

---
+++ ex2_support/ex2_support/base/ex2.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "efm32gg.h"
+
+#define   SAMPLE_PERIOD   0
+
+//====== Declaration of peripheral setup functions ======//
+
+void setupGPIO ();
+void setupTimer (uint32_t period);
+void setupDAC ();
+
+int
+main (void)
+{
+  //====== Call the peripheral setup functions ======//
+
+  setupGPIO ();
+  setupDAC();
+  setupTimer(SAMPLE_PERIOD);
+
+  while (1)
+    {
+      int btnState = *GPIO_PC_DIN;
+      *GPIO_PA_DOUT = btnState << 8;
+    }
+
+  return 0;
+}
