b4cf1f27e7c5a9042d0506b4d28030864189197a
Khakim <khakima@stud.ntnu.no>
2019-10-03 14:22:24 +0200

    Redundant files removed.

--- ex2_support/ex2_support/base/ex2.c
+++ ex2_support/ex2_support/base/ex2.c
@@ -10,6 +10,7 @@
 void setupGPIO ();
 void setupTimer (uint32_t period);
 void setupDAC ();
+void pushToDAC (int sample);
 
 int
 main (void)
@@ -19,11 +20,20 @@ main (void)
   setupGPIO ();
   setupDAC();
   setupTimer(SAMPLE_PERIOD);
-
+	int t = 0;
+	int s = 200;
   while (1)
     {
       int btnState = *GPIO_PC_DIN;
       *GPIO_PA_DOUT = btnState << 8;
+      if (t < 10000) 
+      {
+      	pushToDAC(s);	
+      } else
+      {
+      	t = 0;
+      	if (s > 4000) s = 200; else s = s + 100;
+      }
     }
 
   return 0;
