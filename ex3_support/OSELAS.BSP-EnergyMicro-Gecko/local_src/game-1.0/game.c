8e8716ed9268a43bb21cc05d24cdd2b2464c327c
Khakim <hakim.ahunov@gmail.com>
2019-10-27 20:30:16 +0100

    Basic screen functions added to the game

--- ex3_support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ ex3_support/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -8,100 +8,71 @@
 #include <linux/fb.h>
 #include <sys/ioctl.h>
 
-#define FILESIZE 240 * 640 * sizeof(char) //Size in bytes of the frame buffer
+//====== Local variables ======//
+#define SCREENSIZE	240 * 320
+#define FILESIZE	(SCREENSIZE) * sizeof(short) //Size in bytes of the frame buffer
 
-void lcd_test1();	//Function to test first lcd writting method
-void lcd_test2();	//Function to test second lcd writting method
+int lcdDriver;
+unsigned short* frameBuffer;
+struct fb_copyarea shape;
+
+//====== Local functions ======//
+void initScreen();
+void pushToScreen(unsigned short val, int x, int y);
+void cleanScreen();
 
 int main(int argc, char *argv[])
 {
-    lcd_test2();
+    initScreen();
+    cleanScreen();
+    pushToScreen("ZZ", 5, 5);
+    printf("Test passed!\n");
  	exit(EXIT_SUCCESS);
 }
 
-void lcd_test1() {
-    char *x = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZ"; 	//Line to draw on the lcd
-    char fb_path[20] = "/dev/fb0";				//Path of the lcd driver
-    int result; 								//Error checking variable
+
+//====== LCD screen functions section ======//
+
+void initScreen()
+{
+	lcdDriver = open("/dev/fb0", 2);
+	frameBuffer = (unsigned short*) mmap(NULL, FILESIZE, PROT_WRITE | PROT_READ, MAP_SHARED, lcdDriver, 0);
 	
-	//Open the file
-	int fb = open(fb_path,O_WRONLY);
+	// Default rectangle size
+	shape.width = 16;
+	shape.height = 16;
+}
+
+void pushToScreen(unsigned short val, int x, int y)
+{
+	int a;
+	int b;
 	
-	//Move the place you want to draw
-	lseek(fb,640,SEEK_CUR);
-	
-	//Write to the screen
-	result = write(fb,&x,200);
-	if (result < 0) {
-		printf("Writing error!\n");
+	for (a = y * 16; a < (y*16 + 16); a++) {
+		for (b = x * 16; b < (x*16 + 16); b++) {
+			frameBuffer[a * 320 + b] = val;
+		}
 	}
 
-	//Close the file
-	close(fb);	
-}
-
-void lcd_test2() {
-	int i;
-    int j;
-    int fd;							//File descriptor
-    int result; 					//Error checking variable
-	char fb_path[20] = "/dev/fb0";	//Path of the lcd driver
-	struct fb_copyarea rect;		//Struct to refresh the lcd
+	shape.dx = x * 16;
+	shape.dy = y * 16;
 	
-	//Open the driver file
-    fd = open(fb_path, O_RDWR | O_CREAT | O_TRUNC,(mode_t)0600);
-    if (fd == -1) {
-		perror("Error opening file for writing");
-		exit(EXIT_FAILURE);
-    }
-    
-    //Stretch the file size to the size of the (mmapped) array
-    result = lseek(fd, FILESIZE-2, SEEK_SET);
-    if (result == -1) {
-		close(fd);
-		perror("Error calling lseek() to 'stretch' the file");
-		exit(EXIT_FAILURE);
-    }
-    
-    //Write a value to the end of the file
-    result = write(fd, "", 2);
-    if (result != 2) {
-		close(fd);
-		perror("Error writing last byte of the file");
-		exit(EXIT_FAILURE);
-    }
-
-    //Map the file to an array
-    char *map = mmap(0, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (map == MAP_FAILED) {
-		close(fd);
-		perror("Error mmapping the file");
-		exit(EXIT_FAILURE);
-    }
-    
-    /*Draw something in the array:
-    	-x coordinates go from 0 to 638
-    	-y coordinates go from 0 to 239
-    	-Each pixel is two byes long*/
-    //Draw a square in the array
-    for (i = 0; i <=239; i++) {
-    	for (j = 0; j <=500; j+=2) {
-			map[i*640 + j] = *"ZA"; 
-    	}
-    }
- 
- 	/*Draw desired area into the lcd
- 		-x coordinates go from 0 to 319
-    	-y coordinates go from 0 to 239*/
-    rect.dx=0;
-	rect.dy=0;
-    rect.width=319;
-    rect.height=239;
-    ioctl(fd,0x4680,&rect);
-
-    //Unmap memory
-    if (munmap(map, FILESIZE) == -1) {
-		perror("Error un-mmapping the file");
-    }
+	ioctl(lcdDriver, 0x4680, &shape);
 }
 
+void cleanScreen()
+{
+	shape.dx = 0;
+	shape.dy = 0;
+	shape.width = 320;
+	shape.height = 240;
+	
+	int i;
+	for (i = 0; i < SCREENSIZE; i++) {
+		frameBuffer[i] = 0x0000;
+	}
+	
+	ioctl(lcdDriver, 0x4680, &shape);
+	shape.width = 16;
+	shape.height = 16;
+}
